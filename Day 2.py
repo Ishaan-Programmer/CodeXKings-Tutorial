62423820b2e97afa0e04d0018b1270ec8d9fcdfc
Ishaan-Programmer <ishaanchakrabortty@gmail.com>
2024-12-08 13:22:04 +0530

    Add Day 2 and notes

---
+++ Day 2.py
@@ -0,0 +1,21 @@
+import flask # --- External Module
+import os # --- Built in Module
+
+
+a=5        
+b=3         
+print(a+b)  # --- Do it in terminal use python as a calculator
+
+# Adding two numbers ---- Single Line Comment
+
+'''
+Author:Ishaan
+Class:8
+This is a simple program to add two numbers
+''' # ---- Multi Line Comment
+
+""""
+Author:Ishaan
+Class:8
+This is a simple program to add two numbers
+""" # ---- Multi Line Comment
