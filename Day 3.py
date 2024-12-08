17562eac1b07e47116f44eec97134ee70e0e4b20
Ishaan-Programmer <ishaanchakrabortty@gmail.com>
2024-12-08 21:54:22 +0530

    Add day 3

---
+++ Day 3.py
@@ -0,0 +1,38 @@
+# Problem 1
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+
+'Tis your bright and tiny spark,
+Lights the trav'ller in the dark:
+Tho' I know not what you are,
+Twinkle, twinkle, little star.''')
+
+# Problem 2
+# shown in the terminal
+
+# Problem 3
+import random
+list1 = [1, 2, 3, 4, 5, 6]
+print(random.choice(list1))
+
+# Problem 4
+import flask 
+
+# Problem 5
