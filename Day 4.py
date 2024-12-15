07837aa18c5801dc5c5c46d50704dab7a3358a77
Ishaan-Programmer <ishaanchakrabortty@gmail.com>
2024-12-15 12:49:19 +0530

    Add day 4

---
+++ Day 4.py
@@ -0,0 +1,24 @@
+# Variables 
+a = 10 # ----> a is a variable that stores 10 in the memory
+
+# Data Types
+num = 55 # ----> Integer data type
+decimal = 6.67 # -----> Floating point number data type
+text = "readme" # -----> String data type
+b = True # -----> Boolean data type
+nothing = None # -----> None data type
+
+# Rules for writing Variable
+ishaan = 45 # it doesn't throw error
+# 9ishaan = 10 # it does throw error
+_my_name_is = "Ishaan" # it doesn't throw error
+
+a = 5
+a += 2
+print(a)
+
+b = a!=6
+print(b)
+
+print(not(True))
+
